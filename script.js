9629d79a3d2583b13ae73fba38130955295fdcfc
Nvnchi <r0879363@student.thomasmore.be>
2023-11-03 03:05:51 +0100

    fix: everything is clear now, fixed a whole lot & feat: new pages, cursor works

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function sidereveal() {
   var reveals = document.querySelectorAll(".sidereveallocation");
+  var reveals2 = document.querySelectorAll(".sidereveallocation2");
+  var reveals3 = document.querySelectorAll(".sidereveallocation2");
   var revealstest = document.querySelectorAll(".sidereveal");
+  var revealsactive = document.querySelectorAll(".link")
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 700;
+    var elementVisible = 600;
 
     if (elementTop < windowHeight - elementVisible) {
       revealstest[i].classList.add("active");
+      revealsactive[i].classList.add("gradient");
+      document.getElementById("hartje").src = "./images/hartje_gekleurd.svg"; // Change the image source to "hartje_gekleurd" in the if statement
     } else {
       revealstest[i].classList.remove("active");
+      revealsactive[i].classList.add("gradient");
+      document.getElementById("hartje").src = "./images/hartje_zwart.svg"; // Change the image source to "hartje_zwart" in the else statement
     }
   }
 }
@@ -18,28 +25,5 @@ sidereveal()
 
 window.addEventListener("scroll", sidereveal);
 
-const bigBall = document.querySelector('.cursor__ball--big');
-const smallBall = document.querySelector('.cursor__ball--small');
-const hoverables = document.querySelectorAll('.hoverable');
 
-// Listeners
-document.body.addEventListener('mousemove', onMouseMove);
-hoverables.forEach((element) => {
-  element.addEventListener('mouseenter', onMouseHover);
-  element.addEventListener('mouseleave', onMouseHoverOut);
-});
-
-// Move the cursor
-function onMouseMove(e) {
-  gsap.to(bigBall, { duration: 0.4, x: e.pageX - 15, y: e.pageY - 15 });
-  gsap.to(smallBall, { duration: 0.1, x: e.pageX - 5, y: e.pageY - 7 });
-}
-
-// Hover an element
-function onMouseHover() {
-  gsap.to(bigBall, { duration: 0.3, scale: 4 });
-}
-
-function onMouseHoverOut() {
-  gsap.to(bigBall, { duration: 0.3, scale: 1 });
-}
+            
