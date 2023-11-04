de862ccbc60f1c8a9712ad95b51abc286715b579
Nvnchi <r0879363@student.thomasmore.be>
2023-11-05 00:40:38 +0100

    feat: projects & fix: styling

--- script.js
+++ script.js
@@ -11,43 +11,47 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 function sidereveal() {
   var reveals = document.querySelectorAll(".sidereveallocation");
+  // blahblah
   var reveals2 = document.querySelectorAll(".sidereveallocation2");
   var reveals3 = document.querySelectorAll(".sidereveallocation3");
   var revealstest = document.querySelectorAll(".sidereveal");
   var revealsactive = document.querySelectorAll(".link")
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementTop2 = reveals2[i].getBoundingClientRect().top;
-    var elementTop3 = reveals3[i].getBoundingClientRect().top;
-    var elementVisible = 600;
 
-    if (elementTop3 < windowHeight - elementVisible) {
-      revealstest[i].classList.add("active");
-      revealsactive[i].classList.add("gradient");
-      clear()
-      document.getElementById("link-contact").classList.add("gradient")
-      document.getElementById("link-contact").classList.add("active")
-      document.getElementById("link-contact").children[0].src = "./images/hartje_gekleurd.svg"; // Change the image source to "hartje_gekleurd" in the if statement
-    } else if (elementTop2 < windowHeight - elementVisible) {
-      revealstest[i].classList.add("active");
-      revealsactive[i].classList.add("gradient");
-      clear()
-      document.getElementById("link-projects").classList.add("gradient")
-      document.getElementById("link-projects").classList.add("active")
-      document.getElementById("link-projects").children[0].src = "./images/hartje_gekleurd.svg"; // Change the image source to "hartje_gekleurd" in the if statement
-    } else if (elementTop < windowHeight - elementVisible) {
-      revealstest[i].classList.add("active");
-      revealsactive[i].classList.add("gradient");
-      clear()
-      document.getElementById("link-aboutme").classList.add("gradient")
-      document.getElementById("link-aboutme").classList.add("active")
-      document.getElementById("link-aboutme").children[0].src = "./images/hartje_gekleurd.svg"; // Change the image source to "hartje_gekleurd" in the if statement
-    } else {
-      revealstest[i].classList.remove("active");
-      revealsactive[i].classList.add("gradient");
-      clear()
-    }
+  var windowHeight = window.innerHeight;
+  var elementTop = reveals[0].getBoundingClientRect().top;
+  var elementTop2 = reveals2[0].getBoundingClientRect().top;
+  var elementTop3 = reveals3[0].getBoundingClientRect().top;
+  var elementVisible = 600;
+
+  if (elementTop3 < windowHeight - elementVisible) {
+    revealstest[0].classList.add("active");
+    document.getElementById('top').classList.add("active");
+    revealsactive[0].classList.add("gradient");
+    clear()
+    document.getElementById("link-contact").classList.add("gradient")
+    document.getElementById("link-contact").classList.add("active")
+    document.getElementById("link-contact").children[0].src = "./images/hartje_gekleurd.svg"; // Change the image source to "hartje_gekleurd" in the if statement
+  } else if (elementTop2 < windowHeight - elementVisible) {
+    revealstest[0].classList.add("active");
+    document.getElementById('top').classList.add("active");
+    revealsactive[0].classList.add("gradient");
+    clear()
+    document.getElementById("link-projects").classList.add("gradient")
+    document.getElementById("link-projects").classList.add("active")
+    document.getElementById("link-projects").children[0].src = "./images/hartje_gekleurd.svg"; // Change the image source to "hartje_gekleurd" in the if statement
+  } else if (elementTop < windowHeight - elementVisible) {
+    revealstest[0].classList.add("active");
+    document.getElementById('top').classList.add("active");
+    revealsactive[0].classList.add("gradient");
+    clear()
+    document.getElementById("link-aboutme").classList.add("gradient")
+    document.getElementById("link-aboutme").classList.add("active")
+    document.getElementById("link-aboutme").children[0].src = "./images/hartje_gekleurd.svg"; // Change the image source to "hartje_gekleurd" in the if statement
+  } else {
+    revealstest[0].classList.remove("active");
+    document.getElementById('top').classList.remove("active");
+    revealsactive[0].classList.add("gradient");
+    clear()
   }
 }
 
