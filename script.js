21ecb1f24fa93de166709209bd545f78934cd0cf
Nvnchi <r0879363@student.thomasmore.be>
2023-11-04 01:54:23 +0100

    fix: everything is somewhat clean now

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+window.transitionToPage = function(href) {
+  document.querySelector('body').style.opacity = 0
+  setTimeout(function() { 
+      window.location.href = href
+  }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.querySelector('body').style.opacity = 1
+})
+
 function sidereveal() {
   var reveals = document.querySelectorAll(".sidereveallocation");
   var reveals2 = document.querySelectorAll(".sidereveallocation2");
